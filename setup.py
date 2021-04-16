06f386587f4b63fc6e7e38b6253c58232b693a2f
Brandon Forys <bforys@gmail.com>
2021-04-16 11:51:34 -0700

    Updated numpy requirements to match DLC

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/bf777/mesonet",
-    install_requires=['imutils', 'scikit-image', 'scipy', 'numpy>=1.17.3', 'keras==2.3.1', 'opencv-python==4.4.0.46',
+    install_requires=['imutils', 'scikit-image', 'scipy', 'numpy~=1.17.3', 'keras==2.3.1', 'opencv-python==4.4.0.46',
                       'opencv-contrib-python', 'Pillow', 'deeplabcut', 'pandas', 'matplotlib', 'python-polylabel',
                       'imgaug', 'voxelmorph'],
     packages=['mesonet', ],
